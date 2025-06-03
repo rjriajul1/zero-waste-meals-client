06760dd92b827dda29330e1bbbd3cd759375fc33
Riajul Islam <rjriajul1>
2025-06-03 22:32:33 +0600

    added navbar with responsive all divice

---
+++ src/pages/manageMyFoods/ManageMyFoods.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageMyFoods = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ManageMyFoods;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,14 @@
 import {
   createBrowserRouter,
-  RouterProvider,
 } from "react-router";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/home/Home";
+import AvailableFoods from "../pages/availableFoods/AvailableFoods";
+import AddFood from "../pages/addFood/AddFood";
+import ManageMyFoods from "../pages/manageMyFoods/ManageMyFoods";
+import MyFoodRequest from "../pages/myFoodRequest/MyFoodRequest";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 
 const router = createBrowserRouter([
   {
@@ -13,7 +18,31 @@ const router = createBrowserRouter([
         {
             index: true,
             Component:Home
-        }
+        },
+        {
+            path: 'availableFoods',
+            Component:AvailableFoods
+        },
+        {
+            path: 'addFood',
+            Component:AddFood
+        },
+        {
+            path: 'manageMyFoods',
+            Component:ManageMyFoods
+        },
+        {
+            path: 'myFoodRequest',
+            Component:MyFoodRequest
+        },
+        {
+            path: 'login',
+            Component:Login
+        },
+        {
+            path: 'register',
+            Component:Register
+        },
     ]
   },
 ]);
