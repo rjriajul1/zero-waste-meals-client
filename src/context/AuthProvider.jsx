f76715dd1d7ca2d8d64486cc56e797f9d94d06d7
Riajul Islam <rjriajul1>
2025-06-04 00:04:44 +0600

    added login with password and email implement

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/firebase.config';
 
 const AuthProvider = ({children}) => {
+    const [loading,setLoading] = useState(true);
 
     // register user with password and email 
     const register = (email,password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
-
+ 
+    // login user same password and email 
+    const login = (email,password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
     const userInfo = {
-       register
+        loading,
+       register,
+       login
     }
     return (
         <AuthContext value={userInfo}>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,54 +1,91 @@
 import React from "react";
-import loginAnimated from '../../assets/lotties/login.json'
+import loginAnimated from "../../assets/lotties/login.json";
 import Lottie from "react-lottie";
 import { Link } from "react-router";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
+import { toast } from "react-toastify";
 
 const Login = () => {
-    
-    // lottie animation data 
-    const defaultOptions = {
-        loop: true,
-        animationData: loginAnimated
-    }
+  const { login } = useAuth();
+  // lottie animation data
+  const defaultOptions = {
+    loop: true,
+    animationData: loginAnimated,
+  };
 
-    // handle login form
-    const handleLoginForm = (e) => {
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email,password);
-    }
+  // handle login form
+  const handleLoginForm = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+    // login with same password and email
+    login(email, password)
+      .then((result) => {
+        if (result.user) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your account login successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          
-        <Lottie 
-        options={defaultOptions}
-        width={350}
-        height={350}
-        />
+          <Lottie options={defaultOptions} width={350} height={350} />
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
             <h1 className="text-3xl text-center font-bold">Login now!</h1>
-            <button className="btn"><FcGoogle size={24}/>Login with Google</button>
-            <button className="btn"><FaGithub size={24}/>Login with GitHub</button>
-               <div className="divider text-xl divider-primary">or</div>
+            <button className="btn">
+              <FcGoogle size={24} />
+              Login with Google
+            </button>
+            <button className="btn">
+              <FaGithub size={24} />
+              Login with GitHub
+            </button>
+            <div className="divider text-xl divider-primary">or</div>
             <form onSubmit={handleLoginForm} className="fieldset">
               <label className="label">Email</label>
-              <input type="email" name="email" className="input" placeholder="Email" />
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+              />
               <label className="label">Password</label>
-              <input type="password" name="password" className="input" placeholder="Password" />
+              <input
+                type="password"
+                name="password"
+                className="input"
+                placeholder="Password"
+              />
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
-              <button type="submit" className="btn btn-neutral mt-4">Login</button>
+              <button type="submit" className="btn btn-neutral mt-4">
+                Login
+              </button>
             </form>
-            <p className="text-[18px]">Don't have an account ? please <Link to='/register' className="text-blue-500 underline">Register</Link></p>
+            <p className="text-[18px]">
+              Don't have an account ? please{" "}
+              <Link to="/register" className="text-blue-500 underline">
+                Register
+              </Link>
+            </p>
           </div>
         </div>
       </div>
